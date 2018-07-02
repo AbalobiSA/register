07bd78f066b00fce70126744655523b58d4dc962
Techairos Consulting <techairos@Coffeebeanss-MacBook-Air.local>
2018-07-02 16:34:18 +0200

    Community selection added. BUG: options always displace unique ID even when commanded to display English name for the community. TODO- list of all available communities is currently hard-coded inside a class...to be received from API

--- src/classes/community_info_class.ts
+++ src/classes/community_info_class.ts
@@ -1,17 +1,13 @@
-import{CommunityClass} from "./community_class";
 
 export class CommunityInfoClass{
 
   comm_province             : string="";
+  comm_community            : string ="";
   comm_not_listed           : boolean=false;
   comm_IRP_chosen           : boolean=false;
   comm_commercial_chosen    : boolean=false;
   comm_recreational_chosen  : boolean=false;
   comm_other_chosen         : boolean=false;
-
-  comm_all_communities : CommunityClass[];//an array of all the available 
-
-
 
   constructor(){
 
