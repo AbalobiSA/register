e4380588b9519f806410090795ccef198fa3f44c
Techairos Consulting <techairos@Coffeebeanss-MacBook-Air.local>
2018-07-02 12:16:56 +0200

    Saving most recent version before switching over to admin panel assignment

--- src/classes/community_info_class.ts
+++ src/classes/community_info_class.ts
@@ -1,4 +1,4 @@
-//import {ProvinceClass} from "./province_class";
+import{CommunityClass} from "./community_class";
 
 export class CommunityInfoClass{
 
@@ -7,6 +7,11 @@ export class CommunityInfoClass{
   comm_IRP_chosen           : boolean=false;
   comm_commercial_chosen    : boolean=false;
   comm_recreational_chosen  : boolean=false;
+  comm_other_chosen         : boolean=false;
+
+  comm_all_communities : CommunityClass[];//an array of all the available 
+
+
 
   constructor(){
 
