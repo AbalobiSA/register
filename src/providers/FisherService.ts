cac06d007dd8c7b91cf9e889c9cbf6ade3d59124
Techairos Consulting <techairos@Coffeebeanss-MacBook-Air.local>
2018-07-09 11:56:06 +0200

    Data collected form all the pages has been packed into an object whose fields match those expected by the backend TODO:(1)post the data to a postbin url (2) generate secret file and post to actual endpoint

--- src/providers/FisherService.ts
+++ src/providers/FisherService.ts
@@ -10,12 +10,12 @@ import {CommunityInfoClass}     from "../classes/community_info_class";
 @Injectable()
 export class FisherService {
 
-    POSTBIN_URL = "hhhh";
 
-  // SERVER_URL = "http://localhost:1337";
-  //SERVER_URL = "http://169.239.183.156:1337";
+    REGISTRATION_URL = "";//place a postbin url here first, then replace with actual, then do secret file
+    CHECK_USER_URL = "";//place a postbin url here first, then replace with actual, then do secret file
 
-  constructor(private http: HttpClient, public registree: Registree) {
+
+    constructor(private http: HttpClient, public registree: Registree) {
 
   }
 
@@ -53,16 +53,90 @@ export class FisherService {
   }
 
 
-  //Submit the registree for registration, check if the registration doesn't exist already
-  fisherSubmitRegistration(){
 
-        console.log("Fisher Service Printing Entered Data !!");
-        console.log(this.registree);
+    //Attempt to register the fisher
+    fisherSubmitRegistration(){
+        let fisher = this.parseFisher();//user object to be posted
+        console.log ("This fisher has been created");
+        console.log (fisher);
+
+    }
 
 
+    //parse fisher data to a format congruent to what the backend expects
+    parseFisher(): Object {
+
+   let parsedFisher = {
+        //Fields currently populated
+        "name"                          : this.registree.firstname,
+        "surname"                       : this.registree.surname,
+        "nickname"                      : this.registree.nickname,
+        "password"                      : this.registree.password,
+        "cell"                          : this.registree.cellNo,
+        "gender"                        : this.registree.gender,
+        "id"                            : this.registree.IDnum,
+        "usertype"                      : this.registree.role,
+        "landingsite"                   : this.registree.community,
+        "fisher_license_irp"            : this.registree.IRP_selected,
+        "fisher_license_recreational"   : this.registree.recreational_selected,
+        "permission_local_implementer"  : this.registree.assistant_agreed,
+        "permission_daff"               : this.registree.DAFF_agreed,
+        "preferred_language"            : "English",//is this okay?
+
+
+        //Unpopulated fieilds
+       // "permission_local_implementer": true,
+       /* "__query_params": {},
+        //"fisher_license_irp": true,
+        "boat_engine_hp": 450,
+        "fisher_boat_type": "Bakkie",
+        "device_version": "4.4.2",
+        "device_uuid": "42a469ece0aef206",
+        "birth_date": "2009-07-06T00:00:00.000Z",
+        "boat_name": "Jjjjj",
+        "boat_reg": "55555",
+        //"preferred_language": "English",
+        "device_model": "GT-I9190",
+        //"gender": "male",
+        "email": "tt@test.com",
+        "boat_own": true,
+        "boat_expDate": "2020-07-06T00:00:00.000Z",
+        "fisher_boat": true,
+        "community_not_specified": true,
+        //"password": "abbababa",
+        //"surname": "Test surname",
+        //"name": "Joshua",
+        "filter": "abalobi_registration",
+        //"usertype": "fisher",
+        "uuid_timestamp": "2018-07-06T14:37:30.684Z",
+        "device_manufacturer": "samsung",
+        "boat_has_engine": true,
+        "fisher_shore": true,
+        "device_platform": "Android",
+        //"permission_daff": true,
+        "boat_engine_cc": 45,
+        //"nickname": "Tatenda",
+        //"fisher_license_recreational": false,
+        //"cell": "0123456789",
+        "app_version": "0.5.5",
+        //"id": "1234567890123",
+        "fisher_licence_irp_number": "Haai",
+        "email_is_my_own": true,
+        "landingsite_custom": "testcom",
+        "device_serial": "002ff2ed"*/
+    }//end method parseUser
+
+    return parsedFisher;//return the user in a format ready for posting
+
+}
+
+
+
+  //Clear the recently entered fisher info
+  fisherClearDetails(){
         //Reset the registree fields after attempted registration
       //Role details
-     /* this.registree.role  = "";
+      this.registree.role  = "";
 
       //Terms of use details
       this.registree.terms_agreed      =false;
@@ -86,17 +160,18 @@ export class FisherService {
       this.registree.commercial_selected   =false;
       this.registree.recreational_selected =false;
       this.registree.other_seleted         =false;
-*/
+
+      console.log("........Fisher Service Cleared User Details........");
   }
 
-  //Check if user with th proposed username doesnt exist already
-  checkIfFisherAlreadyExists(username: string): Promise<any> {
-        return this.http.get(this.POSTBIN_URL + "/api/users/find/?username=" + username).toPromise();
+  //Check if fisher with th proposed username doesnt exist already
+  checkIfFisherAlreadyExists(ID: string): Promise<any> {
+        return this.http.get(this.CHECK_USER_URL + ID).toPromise();
   }
 
-  //Go ahead and actually try to register the user
-  registerFisher(user): Promise<any> {
-        return this.http.post(this.POSTBIN_URL + "/api/users/create/", user).toPromise();
+  //Go ahead and actually try to register the fisher
+  registerFisher(fisher): Promise<any> {
+        return this.http.post(this. REGISTRATION_URL, fisher).toPromise();
   }
 
 
